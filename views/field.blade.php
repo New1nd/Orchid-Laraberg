4ee26f7376d90d51e3ee5ca99e1a8c0d30133e05
Nikita Belov <nickibelov@yandex.ru>
2023-10-30 23:47:09 +0500

    change composer.json

--- views/field.blade.php
+++ views/field.blade.php
@@ -5,4 +5,10 @@
     <script src="https://unpkg.com/react@17.0.2/umd/react.production.min.js"></script>
     <script src="https://unpkg.com/react-dom@17.0.2/umd/react-dom.production.min.js"></script>
     <script src="{{ asset('vendor/laraberg/js/laraberg.js') }}"></script>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            Laraberg.init('{{ $id }}');
+        })
+
+    </script>
 @endcomponent
