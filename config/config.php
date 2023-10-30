bed192c76269ede359544c5d8a43af7638f37126
Nikita Belov <nickibelov@yandex.ru>
2023-10-30 22:27:14 +0500

    init laraberg

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+return [
+    /**
+     * Скрипты загрузки редактора
+     * @link https://ckeditor.com/ckeditor-4/download/
+     */
+    'editorUrl' => '//cdn.ckeditor.com/4.16.2/full/ckeditor.js',
+
+    /**
+     * Настройки редактора по-умолчанию
+     * Ниже приведены настройки для подключения файлового
+     * менеджера на основе "unisharp/laravel-filemanager:^2.2"
+     *
+     * @link https://ckeditor.com/docs/ckeditor4/latest/
+     */
+    'options' => [
+
+    ]
+];
