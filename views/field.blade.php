108835d16c40cb5d3b57869863dd3ffea60e60a3
Nikita Belov <nickibelov@yandex.ru>
2023-10-30 23:54:30 +0500

    change composer.json

--- views/field.blade.php
+++ views/field.blade.php
@@ -1,5 +1,6 @@
 @component($typeForm, get_defined_vars())
     <link rel="stylesheet" href="{{asset('vendor/laraberg/css/laraberg.css')}}">
+    {{ $value ?? '' }}
     <textarea id="{{ $id }}" name="{{ $name }}" hidden>{{ $value ?? '' }}</textarea>
 
     <script src="https://unpkg.com/react@17.0.2/umd/react.production.min.js"></script>
